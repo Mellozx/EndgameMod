142a0020e69cbf9c9ca4b86fdbf6a32e1e85caee
Mellozx <67687232+Mellozx@users.noreply.github.com>
2020-08-08 12:01:13 +0300

    Added First Prototype of the Stormbreaker
    Stuff it does:
    Simple Boomerang
    Can be used with both leftclick and rightclick
    what it needs to do:
    Shoot Lightning
    Faster Speed Rate

---
+++ Items/StormBreaker/StormBreakerProj.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EndgameMod.Items.StormBreaker
+{
+    public class StormBreakerProj : ModProjectile // Simple Boomerang Projectile, Needs to Shoot Lightning and be faster in later development.
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Storm Breaker");
+        }
+        
+
+        public override void SetDefaults()
+        {
+            projectile.width = 30;
+            projectile.height = 30;
+
+            projectile.friendly = true;
+            projectile.thrown = true;
+
+            projectile.penetrate = 6;
+
+            projectile.aiStyle = 3;
+            projectile.timeLeft = 3600;
+            aiType = 52;
+        }
+
+    }
+}
+        
+
